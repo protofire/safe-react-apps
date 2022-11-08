f6dcad871cbf2aec4241df4f9b5100e0ea637f02
the-amazing-ultraton <106994129+the-amazing-ultraton@users.noreply.github.com>
2022-11-08 14:18:46 -0300

    fix: #GSD-1105 - replace gnosis gateway by client-specific production gateways

--- apps/tx-builder/src/lib/getAbi.ts
+++ apps/tx-builder/src/lib/getAbi.ts
@@ -21,12 +21,92 @@ const getProviderURL = (chain: string, address: string, urlProvider: PROVIDER): 
     case PROVIDER.SOURCIFY:
       return `https://sourcify.dev/server/files/${chain}/${address}`
     case PROVIDER.GATEWAY:
-      return `https://safe-client.gnosis.io/v1/chains/${chain}/contracts/${address}`
+      return getGatewayProvider(chain, address)
     case PROVIDER.BLOCKSCOUT:
       return `https://blockscout.com/${chain}/api?module=contract&action=getabi&address=${address}`
     default:
       throw new Error('The Provider is not supported')
   }
+}
+
+const SUPPORTED_CHAINS = {
+  ACALA: '787',
+  KARURA: '686',
+  MANDALA: '595',
+  ASTAR: '592',
+  BOBABEAM: '1294',
+  CRONOS: '25',
+  CRONOS_TESTNET: '338',
+  HARMONY: '1666600000',
+  EVMOS: '9001',
+  EVMOS_TESTNET: '9000',
+  MOONBEAM: '1284',
+  MOONRIVER: '1285',
+  MOONBASE: '1287',
+  TELOS: '40',
+  TELOS_TESTNET: '41',
+  VELAS: '106',
+  VELAS_TESTNET: '111',
+}
+
+const getGatewayProvider = (chain: string, address: string): string => {
+  let url: string
+  switch (chain) {
+    case SUPPORTED_CHAINS.ACALA:
+      url = `https://gateway.safe.acala.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.KARURA:
+      url = `https://gateway.safe.acala.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.MANDALA:
+      url = `https://gateway.safe.acala.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.ASTAR:
+      url = `https://gateway.safe.astar.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.BOBABEAM:
+      url = `https://gateway.multisig.bobabeam.boba.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.CRONOS:
+      url = `https://gateway.cronos-safe.org/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.CRONOS_TESTNET:
+      url = `https://gateway.cronos-safe.org/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.EVMOS:
+      url = `https://gateway.safe.evmos.org/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.EVMOS_TESTNET:
+      url = `https://gateway.safe.evmos.org/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.HARMONY:
+      url = `https://gateway.staging-safe.harmony.one/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.MOONBEAM:
+      url = `https://gateway.multisig.moonbeam.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.MOONRIVER:
+      url = `https://gateway.multisig.moonbeam.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.MOONBASE:
+      url = `https://gateway.multisig.moonbeam.network/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.TELOS:
+      url = `https://gateway.safe.telos.net/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.TELOS_TESTNET:
+      url = `https://gateway.safe.telos.net/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.VELAS:
+      url = `https://gateway.velasafe.com/v1/chains/${chain}/contracts/${address}`
+      break
+    case SUPPORTED_CHAINS.VELAS_TESTNET:
+      url = `https://gateway.velasafe.com/v1/chains/${chain}/contracts/${address}`
+      break
+    default:
+      url = `https://safe-client.gnosis.io/v1/chains/${chain}/contracts/${address}`
+  }
+  return url
 }
 
 const getAbiFromSourcify = async (address: string, chainId: string): Promise<any> => {
