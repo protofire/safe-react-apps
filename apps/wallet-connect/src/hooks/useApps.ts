d11ee9eefc05db23246e31ef9c0b66de366285d5
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2023-01-26 19:41:50 +0300

    refactor: remove trailing slashes from gateway url

--- apps/wallet-connect/src/hooks/useApps.ts
+++ apps/wallet-connect/src/hooks/useApps.ts
@@ -7,89 +7,86 @@ import {
 } from '@safe-global/safe-gateway-typescript-sdk'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
 
+// const BASE_URL = 'https://safe-client.gnosis.io'
+enum SUPPORTED_CHAINS {
+  ACALA = '787',
+  KARURA = '686',
+  MANDALA = '595',
+  ASTAR = '592',
+  SHIDEN = '336',
+  SHIBUYA = '81',
+  BOBABEAM = '1294',
+  CRONOS = '25',
+  CRONOS_TESTNET = '338',
+  EVMOS = '9001',
+  EVMOS_TESTNET = '9000',
+  HARMONY = '1666600000',
+  HARMONY_TESTNET = '1666700000',
+  MOONBEAM = '1284',
+  MOONRIVER = '1285',
+  MOONBASE = '1287',
+  TELOS = '40',
+  TELOS_TESTNET = '41',
+  THUNDER_CORE = '108',
+  VELAS = '106',
+  VELAS_TESTNET = '111',
+}
+
+const getGatewayBaseUrl = (chain: string) => {
+  const isProdEnv = process.env?.REACT_APP_IS_PRODUCTION === 'true'
+
+  switch (chain) {
+    case SUPPORTED_CHAINS.ACALA:
+    case SUPPORTED_CHAINS.KARURA:
+    case SUPPORTED_CHAINS.MANDALA:
+      return isProdEnv
+        ? `https://gateway.safe.acala.network`
+        : `https://gateway.staging.safe.acala.network`
+    case SUPPORTED_CHAINS.ASTAR:
+    case SUPPORTED_CHAINS.SHIDEN:
+    case SUPPORTED_CHAINS.SHIBUYA:
+      return isProdEnv
+        ? `https://gateway.safe.astar.network`
+        : `https://gateway.staging-safe.astar.network`
+    case SUPPORTED_CHAINS.BOBABEAM:
+      return isProdEnv
+        ? `https://gateway.multisig.bobabeam.boba.network`
+        : `https://gateway.staging.multisig.bobabeam.boba.network`
+    case SUPPORTED_CHAINS.CRONOS:
+    case SUPPORTED_CHAINS.CRONOS_TESTNET:
+      return isProdEnv
+        ? `https://gateway.cronos-safe.org`
+        : `https://gateway-cronos-safe.crolabs-int.co`
+    case SUPPORTED_CHAINS.EVMOS:
+    case SUPPORTED_CHAINS.EVMOS_TESTNET:
+      return isProdEnv ? `https://gateway.safe.evmos.org` : `https://gateway.safe.evmos.dev`
+    case SUPPORTED_CHAINS.HARMONY:
+    case SUPPORTED_CHAINS.HARMONY_TESTNET:
+      return isProdEnv
+        ? `https://gateway.multisig.harmony.one`
+        : `https://gateway.staging-safe.harmony.one`
+    case SUPPORTED_CHAINS.MOONBEAM:
+    case SUPPORTED_CHAINS.MOONRIVER:
+    case SUPPORTED_CHAINS.MOONBASE:
+      return isProdEnv
+        ? `https://gateway.multisig.moonbeam.network`
+        : `https://gateway.staging.multisig.moonbeam.network`
+    case SUPPORTED_CHAINS.TELOS:
+    case SUPPORTED_CHAINS.TELOS_TESTNET:
+      return `https://gateway.safe.telos.net`
+    case SUPPORTED_CHAINS.THUNDER_CORE:
+      return isProdEnv ? `` : `https://gateway.staging.safe.thundercore.com`
+    case SUPPORTED_CHAINS.VELAS:
+    case SUPPORTED_CHAINS.VELAS_TESTNET:
+      return isProdEnv ? `https://gateway.velasafe.com` : `https://gateway.staging.velasafe.com`
+    default:
+      throw new Error('UNSUPPORTED_CHAIN')
+  }
+}
+
 type UseAppsResponse = {
   findSafeApp: (safeAppUrl: string) => SafeAppData | undefined
   openSafeApp: (safeAppUrl: string) => void
-}
-
-const SUPPORTED_CHAINS = {
-  ACALA: '787',
-  KARURA: '686',
-  MANDALA: '595',
-  ASTAR: '592',
-  BOBABEAM: '1294',
-  CRONOS: '25',
-  CRONOS_TESTNET: '338',
-  HARMONY: '1666600000',
-  EVMOS: '9001',
-  EVMOS_TESTNET: '9000',
-  MOONBEAM: '1284',
-  MOONRIVER: '1285',
-  MOONBASE: '1287',
-  TELOS: '40',
-  TELOS_TESTNET: '41',
-  VELAS: '106',
-  VELAS_TESTNET: '111',
-}
-
-const getGatewayBaseUrl = (chain: string): string => {
-  let url: string
-  switch (chain) {
-    case SUPPORTED_CHAINS.ACALA:
-      url = `https://gateway.safe.acala.network/`
-      break
-    case SUPPORTED_CHAINS.KARURA:
-      url = `https://gateway.safe.acala.network/`
-      break
-    case SUPPORTED_CHAINS.MANDALA:
-      url = `https://gateway.safe.acala.network/`
-      break
-    case SUPPORTED_CHAINS.ASTAR:
-      url = `https://gateway.safe.astar.network/`
-      break
-    case SUPPORTED_CHAINS.BOBABEAM:
-      url = `https://gateway.multisig.bobabeam.boba.network/`
-      break
-    case SUPPORTED_CHAINS.CRONOS:
-      url = `https://gateway.cronos-safe.org/`
-      break
-    case SUPPORTED_CHAINS.CRONOS_TESTNET:
-      url = `https://gateway.cronos-safe.org/`
-      break
-    case SUPPORTED_CHAINS.EVMOS:
-      url = `https://gateway.safe.evmos.org/`
-      break
-    case SUPPORTED_CHAINS.EVMOS_TESTNET:
-      url = `https://gateway.safe.evmos.org/`
-      break
-    case SUPPORTED_CHAINS.HARMONY:
-      url = `https://gateway.staging-safe.harmony.one/`
-      break
-    case SUPPORTED_CHAINS.MOONBEAM:
-      url = `https://gateway.multisig.moonbeam.network/`
-      break
-    case SUPPORTED_CHAINS.MOONRIVER:
-      url = `https://gateway.multisig.moonbeam.network/`
-      break
-    case SUPPORTED_CHAINS.MOONBASE:
-      url = `https://gateway.multisig.moonbeam.network/`
-      break
-    case SUPPORTED_CHAINS.TELOS:
-      url = `https://gateway.safe.telos.net/`
-      break
-    case SUPPORTED_CHAINS.TELOS_TESTNET:
-      url = `https://gateway.safe.telos.net/`
-      break
-    case SUPPORTED_CHAINS.VELAS:
-      url = `https://gateway.velasafe.com/`
-      break
-    case SUPPORTED_CHAINS.VELAS_TESTNET:
-      url = `https://gateway.velasafe.com/`
-      break
-    default:
-      throw new Error('unsupported chain')
-  }
-  return url
 }
 
 export function useApps(): UseAppsResponse {
