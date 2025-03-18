ae5037c333ffb9211f140213a8430196a94e2a06
Denis Smolonski <denissmolonski@gmail.com>
2025-03-18 15:49:50 +0100

    feat: add berachain

--- apps/tx-builder/src/lib/getAbi.ts
+++ apps/tx-builder/src/lib/getAbi.ts
@@ -424,6 +424,7 @@ const getScanAPIBaseURL = (chain: string): undefined | { link: string; apiKey?: 
     case SUPPORTED_CHAINS.BERACHAIN:
       return {
         link: 'https://beratrail.io',
+        apiKey: process.env.REACT_APP_BERASCAN_KEY,
       }
     default:
       return
